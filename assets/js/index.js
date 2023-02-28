a462ec8269e47e19790e1121c027a4b05d3d8ee3
francogodoy92 <franck92elrojo@gmail.com>
2023-02-28 20:56:03 -0300

    Begins

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-let events = data.events;
+const events = data.events;
 
 const cards = document.createDocumentFragment();
 
@@ -6,6 +6,7 @@ let container = document.getElementById("container-cards");
 
 function imprimirCard(array, contenedor) {
   for (let event of array) {
+    cards.innerHTML = "";
     let div = document.createElement("div");
     div.className = "card col-md-3 m-3";
     div.innerHTML += `
@@ -22,27 +23,34 @@ function imprimirCard(array, contenedor) {
   contenedor.appendChild(cards);
 }
 
-imprimirCard(events, container)
+imprimirCard(events, container);
 
 let categoryContainer = document.getElementById("checkbox");
 let categoryContainerPadre = document.getElementById("form-father");
 let categorias = Array.from(
-  new Set(events.map(elemento => elemento.category))
+  new Set(events.map((elemento) => elemento.category))
 );
 
 const check = document.createDocumentFragment();
 
 function renderCheckboxs(category) {
   let checkboxs = document.createElement("fieldset");
-  category.forEach(element => {
+  category.forEach((element) => {
     checkboxs.innerHTML += `<label class="text-white m-2" for="${element}">${element}</label>
     <input type="checkbox" name="category" value="${element}" id="${element}">`;
     check.appendChild(checkboxs);
-});
-categoryContainer.appendChild(check);
+  });
+  categoryContainer.appendChild(check);
 }
 renderCheckboxs(categorias);
 
-
-
-
+/* function filtrarPorCategoria(eventos){
+  let checked = (Array.from(document.querySelectorAll("input[type ='checkbox']:checked")).map(element => element.value));
+  let arrayFiltrado = checked.map(value => eventos.filter(elemento => {elemento.category === value})).flat();
+  if (checked.length == false) {
+    return events;
+  } else {
+    return arrayFiltrado;
+  }
+}
+ */
