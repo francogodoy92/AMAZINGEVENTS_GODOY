5ced8a4c8d7d0ccc9cc664f6efe39b51b5db538e
francogodoy92 <franck92elrojo@gmail.com>
2023-03-14 17:56:00 -0300

    Task4

--- assets/js/pastEvents.js
+++ assets/js/pastEvents.js
@@ -102,34 +102,3 @@ function filtrar() {
   );
   return filtradoPorBusqueda;
 }
-
-/* import data from "./amazing.js";
-
-let events = data.events;
-
-const cards = document.createDocumentFragment();
-
-let container = document.getElementById("container-cards");
-
-let oldEvents = events.filter((events) => events.date < data.currentDate);
-
-function imprimirCard(array, contenedor) {
-    for (let event of array) {
-        let div = document.createElement("div");
-        div.className = "card col-md-3 m-3";
-        div.innerHTML += `
-        <img src="${event.image}" alt="Event Image">
-        <h3>${event.name}</h3>   
-        <p>${event.category}</p>
-        <div class="item-card">
-            <p>$${event.price}</p>
-            <a class="btn" href="./pages/details.html">View More</a>
-        </div>
-        `;
-        cards.appendChild(div);
-    }
-    contenedor.appendChild(cards);
-}
-
-imprimirCard(oldEvents, container);
- */
