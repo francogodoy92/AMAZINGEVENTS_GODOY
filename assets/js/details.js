3c1e3e9ccd53b014002437cc40b1fb4c75dca8de
francogodoy92 <franck92elrojo@gmail.com>
2023-03-14 13:47:22 -0300

    Task4

--- assets/js/details.js
+++ assets/js/details.js
@@ -12,10 +12,12 @@ const details = document.getElementById("details-conteiner");
 
 details.innerHTML = `<img src="${eventsId.image}" alt="${eventsId.name}">
 <div>
-    <h2 class="detailstitle">${eventsId.name}</h2>
-    <p class="detailstext">${eventsId.description}</p>
-    <p class="detailstext">${eventsId.place}</p>
-    <p class="detailstext">${eventsId.date}</p>
-    <p class="detailsprice">$${eventsId.price}</p>
+    <h2 class="detailstitle"><span style="font-weight: bold; text-decoration: underline;">Event</span>: ${eventsId.name}</h2>
+    <p class="detailstext"><span style="font-weight: bold; text-decoration: underline;">Category</span>: ${eventsId.category}</p>
+    <p class="detailstext"><span style="font-weight: bold; text-decoration: underline;">Description</span>: ${eventsId.description}</p>
+    <p class="detailstext"><span style="font-weight: bold; text-decoration: underline;">Place</span>: ${eventsId.place}</p>
+    <p class="detailstext"><span style="font-weight: bold; text-decoration: underline;">Date</span>: ${eventsId.date}</p>
+    <p class="detailstext"><span style="font-weight: bold; text-decoration: underline;">Capacity</span>: ${eventsId.capacity}</p>
+    <p class="detailsprice"><span style="font-weight: bold; text-decoration: underline;">Price</span>: $${eventsId.price}</p>
 </div>
 `;
--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,109 +1,168 @@
-let tdUno = document.getElementById("tdUno")
-let tdDos = document.getElementById("tdDos")
-let tdTres = document.getElementById("tdTres")
+let dataApi = [];
+let dataArray;
+async function getDataFromApi() {
+  await fetch(`../assets/amazing.json`)
+    .then(response => response.json())
+    .then(json => dataApi = json)
+  dataArray = dataApi.events
+  console.log(dataArray)
 
+  let cardDate = dataApi.currentDate
+  let arrayPast = dataArray.filter(e => cardDate > e.date)
+  let arrayFuture = dataArray.filter(e => cardDate < e.date)
+  console.log(arrayPast)
+  console.log(arrayFuture)
 
-
-fetch('../assets/amazing.json')
-.then(response => response.json())
-.then(data => {
-    const datos = data
-    console.log(datos)
-    mayorPorcentaje(datos.events, tdUno)
-    menorPorcentaje(datos.events, tdDos)
-    mayorCapacidad(datos.events, tdTres)
-    let upcomingEvents = (datos.events).filter(function (event) {
-        return event.date >= data.currentDate;
-      });
-      let pastEvents = (datos.events).filter(function (event) {
-        return event.date < data.currentDate;
-      });
-      console.log(pastEvents)
-      console.log(upcomingEvents)
-})
-.catch(error => console.error(error));
-
-function mayorPorcentaje(array, container){
-    let arrayConAsistencias = [...array.filter(element => element.assistance)]
-    let arrayMayorPorcentaje = []
-    arrayConAsistencias.forEach(element => {
-        let porcentaje = element.assistance / element.capacity * 100 
-        console.log(porcentaje)
-        if (porcentaje > 94){
-            return arrayMayorPorcentaje.push(`${element.name}`)
-        }
+    //PRIMER TABLA
+    let porcentajes = []
+    arrayPast.map(eventos => {
+      porcentajes.push({
+        eventos: eventos.name,
+        porAssistance: (eventos.assistance * 100 / eventos.capacity).toFixed(2)
+      })
     })
-    
-    console.log(arrayMayorPorcentaje)
-    container.innerHTML = `<td>${arrayMayorPorcentaje}</td>`
-}
+    let MAX = porcentajes.sort((a, b) => b.porAssistance - a.porAssistance)[0]
+    console.log(MAX)
+    let MIN = porcentajes.sort((a, b) => a.porAssistance - b.porAssistance)[0]
+    console.log(MIN)
+    let capacity = arrayPast.filter(e => e.capacity).sort((a, b) => b.capacity - a.capacity)[0]
+    console.log(capacity)
+  
+   //EVENTOS FUTUROS 
+   const categoryAssistFuture = arrayFuture.map(eventos => eventos.category)
+   const categorySetFuture = new Set(categoryAssistFuture)
+   const categorysFuture = [...categorySetFuture]
+   console.log(categorysFuture)
+ 
+   const categoryValueFuture = [] //Creamos un Array que contiene 1 objeto con 2 propiedades
+   categorysFuture.map(category =>
+     categoryValueFuture.push({
+       category: category,
+       evento: arrayFuture.filter(evento => evento.category === category), //Ahora tenemos las categorias que tienen adentro todos los eventos pasados
+     }))
+   console.log(categoryValueFuture)
+ 
+   let estimateAndCapacityFuture = [] // De la varible anterior mapeamos en un nuevo array,
+   categoryValueFuture.map(datos => {
+     estimateAndCapacityFuture.push({
+       category: datos.category,
+       estimate: datos.evento.map(item => item.estimate),
+       capacity: datos.evento.map(item => item.capacity),
+       estimateRevenue: datos.evento.map(item => item.estimate * item.price)
+     })
+   })
+   console.log(estimateAndCapacityFuture)
+ 
+   estimateAndCapacityFuture.forEach(category => {
+     let totalEstimate = 0
+     category.estimate.forEach(estimate => totalEstimate += Number(estimate)) //suma de assistencia
+     category.estimate = totalEstimate
+ 
+     let totalCapacityFut = 0
+     category.capacity.forEach(capacity => totalCapacityFut += Number(capacity)) //suma de capacity
+     category.capacity = totalCapacityFut
+ 
+     let totalEstimateRevenue = 0
+     category.estimateRevenue.forEach(estimateRevenue => totalEstimateRevenue += Number(estimateRevenue)) //suma de revenue
+     category.estimateRevenue = totalEstimateRevenue
+ 
+     category.porcentajeAttendace = ((totalEstimate * 100) / totalCapacityFut).toFixed(2) //le agregamos una nueva propiedad, el calculo de % assistencia total por categoria.
+   })
+   console.log(estimateAndCapacityFuture)
+ 
 
-function menorPorcentaje(array, conteiner){
-    let arrayConAsistencias = [...array.filter(element => element.assistance)]
-    let arrayMenorPorcentaje = []
-    arrayConAsistencias.forEach(element => {
-        let porcentaje = element.assistance / element.capacity * 100 
-        console.log(porcentaje)
-        if (porcentaje < 70){
-            return arrayMenorPorcentaje.push(`${element.name}`)
-        }
-    })
-    
-    console.log(arrayMenorPorcentaje)
-    conteiner.innerHTML = `<td>${arrayMenorPorcentaje}</td>`
 
-}
+   //EVENTOS PASADOS
+   
+   const categoryAssit = arrayPast.map(eventos => eventos.category) // Extrajimos las categorias del array del evento pasado
+   const categorySet = new Set(categoryAssit) //Aplicamos el sett para eliminar las categorias duplicadas
+   const categorys = [...categorySet] //Ahora en categorys tenemos un array de 7 categorias
+   console.log(categorys)
+ 
+   const categoryValue = [] //Creamos un Array que contiene 1 objeto con 2 propiedades
+   categorys.map(category =>
+     categoryValue.push({
+       category: category,
+       evento: arrayPast.filter(evento => evento.category === category), //Ahora tenemos las categorias que tienen adentro todos los eventos pasados
+     })
+   )
+   console.log(categoryValue)
+ 
+ 
+   let assistAndCapacityPast = [] // De la varible anterior mapeamos en un nuevo array,
+   categoryValue.map(datos => {
+     assistAndCapacityPast.push({
+       category: datos.category,
+       assistance: datos.evento.map(item => item.assistance),
+       capacity: datos.evento.map(item => item.capacity),
+       revenue: datos.evento.map(item => item.assistance * item.price)
+     })
+   })
+   console.log(assistAndCapacityPast)
+   //Ahora sumamos todos los elementos de cada propiedad entre si (assistance, capacity, revenue)
+ 
+   assistAndCapacityPast.forEach(category => {
+     let totalAssit = 0
+     category.assistance.forEach(assistance => totalAssit += Number(assistance)) //suma de assistencia
+     category.assistance = totalAssit
+ 
+     let totalCapacity = 0
+     category.capacity.forEach(capacity => totalCapacity += Number(capacity)) //suma de capacity
+     category.capacity = totalCapacity
+ 
+     let totalRevenue = 0
+     category.revenue.forEach(revenue => totalRevenue += Number(revenue)) //suma de revenue
+     category.revenue = totalRevenue
+ 
+     category.porcentaje = ((totalAssit * 100) / totalCapacity).toFixed(2) //le agregamos una nueva propiedad, el calculo de % assistencia total por categoria.
+   })
+   console.log(assistAndCapacityPast)
+   console.log(categoryValue)
+ 
+   /**
+    * *IMPRIMIR TABLAS
+    */
+   function PrintTableOne() {
+     let contenedorUno = ` <tr>     
+     <td>${MAX.eventos}: ${MAX.porAssistance}%</td>
+                          <td>${MIN.eventos}: ${MIN.porAssistance}%</td>
+                          <td>${capacity.name}: ${capacity.capacity}</td>
+                          </tr>`
+     document.getElementById("tablaEventsStadistics").innerHTML = contenedorUno
+   }
+   PrintTableOne()
+ 
+   function PrintTableTwo() {
+     let contenedorDos = ``;
+     estimateAndCapacityFuture.forEach(e => {
+       e.estimateAndCapacityFuture
+       contenedorDos += `<tr>
+      <td>${e.category}</td>
+      <td>$${e.estimateRevenue}</td>
+      <td>${e.porcentajeAttendace}%</td>
+    </tr>`
+     })
+     document.getElementById("tablaUpcomingEvents").innerHTML = contenedorDos
+   }
+   PrintTableTwo()
+ 
+   function PrintTableThree() {
+     let contenedorTres = ``;
+     assistAndCapacityPast.forEach(e => {
+       e.assistAndCapacityPast
+       contenedorTres += `<tr>
+      <td>${e.category}</td>
+      <td>$${e.revenue}</td>
+      <td>${e.porcentaje}%</td>
+    </tr>`
+     })
+     document.getElementById("tablaPastEvents").innerHTML = contenedorTres
+   }
+   PrintTableThree()
+ 
+ 
+ }
+ getDataFromApi()
 
-function mayorCapacidad(array, conteiner) {
-  let mayorCapacidad = 0;
-  let eventoConMayorCapacidad = '';  
-  array.forEach(element => {
-    if (element.capacity > mayorCapacidad) {
-      mayorCapacidad = element.capacity;
-      eventoConMayorCapacidad = element.name;
-    }
-  });
-
-  conteiner.innerHTML = `<td>${eventoConMayorCapacidad}</td>`;
-}
-
-let upcomingContainer = document.getElementById("upcoming-container")
-let pastContainer = document.getElementById("past-container")
 
 
-
-
-
-
-
-
-
-/* function crearCategoriasTablaUpcoming(events, container) {
-  let html = '';
-  events.forEach(function(cat) {
-    html += `
-      <tr>
-        <td>${cat.category}</td>
-        <td>${cat.estimate * cat.price}</td>
-        <td>${((cat.estimate * 100 )/ cat.capacity).toFixed(2)}</td>
-      </tr>
-    `;
-  });
-  container.innerHTML = html;
-}
- */
-
-/* function crearCategoriasTablaUpcoming(category, container) {
-    let html = '';
-    for (let i = 0; i < category.length; i++) {
-      html += `
-        <tr>
-          <td>${category[i].category}</td>
-          <td>${category[i].estimate * category[i].price}</td>
-          <td>${((category[i].assistance * 100 )/ category[i].capacity).toFixed(2)}</td>
-        </tr>
-      `;
-    }
-    container.innerHTML = html;
-  }
-   */
